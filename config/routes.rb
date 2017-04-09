ba0d8aac37fc2b9f094eeb2ca3295269f062af48
tech-camp <tetsu.ktm@gmail.com>
2017-04-09 21:43:02 +0900

    http://easyramble.com/create-users-index-show-on-devise.htmlを参照

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords",
+  }
+  resources :users, :only => [:index, :show]
   get 'solutions' => 'solutions#index'
   get 'solutions/new' => 'solutions#new'
   post 'solutions' => 'solutions#create'
