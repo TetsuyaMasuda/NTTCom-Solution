e21fa29df6e4c8ed5bbeb0971862e39250a95141
tech-camp <tetsu.ktm@gmail.com>
2017-04-09 20:49:47 +0900

    device_for :usersの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
   get 'solutions' => 'solutions#index'
   get 'solutions/new' => 'solutions#new'
   post 'solutions' => 'solutions#create'
